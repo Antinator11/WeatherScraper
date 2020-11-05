c9f1d7d6699ff663ed8dd6f852ec40022e6e926f
VOTEJH <jhv003@hotmail.com>
2020-11-05 11:22:37 +1000

    Added start of Scraper and imported some packages

--- BOMScraper.py
+++ BOMScraper.py
@@ -7,25 +7,30 @@ Page = requests.get(url)
 Soup = BeautifulSoup(Page.text, 'html.parser')
 outputlist = []
 
-# find title based on tag
+
+# find location based on title
+Location = Soup.find('title').text
+outputlist.append(Location)
+
+# find Today based on class
 today = Soup.find('h2', class_="pointer").text
 outputlist.append(today)
 
-# find price based on tag
-price = Soup.find('span', class_ ="sale-price").text
-outputlist.append(price)
+# find table based on summary
+TempTable = Soup.find('table', summary ="3 Hourly Temperatures Forecast for Thursday").text.strip("")
+outputlist.append(TempTable)
 
 # find date published based on tag
-date = Soup.find('span', itemprop ="datePublished").text
-outputlist.append(date)
+# date = Soup.find('span', itemprop ="datePublished").text
+# outputlist.append(date)
 
 # find image address based on tag
-img = Soup.find('img', itemprop="image")['src']
-outputlist.append(img)
+# img = Soup.find('img', itemprop="image")['src']
+# outputlist.append(img)
 
 # find rating based on tag
-rating = Soup.find('span', itemprop="ratingValue").text.strip()
-outputlist.append(rating)
+# rating = Soup.find('span', itemprop="ratingValue").text.strip()
+# outputlist.append(rating)
 
 # ------------------------Book SCaper Stuff. WIll fix later ----------- #
 # def addbook(bookdata):
@@ -38,11 +43,12 @@ outputlist.append(rating)
     # connect.close()
 
 
+print(Location)
 print(today)
-print(price)
-print(date)
-print(img)
-print(rating)
+print(TempTable)
+# print(date)
+# print(img)
+# print(rating)
 
 # outputtuple = tuple(outputlist)
 # addbook(outputtuple)
